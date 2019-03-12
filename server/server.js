7d5fd74d86bb601735e2663b55bf776162ce9894
Michal Kubiče <kubicemichal@gmail.com>
2019-03-12 14:56:50 +0100

    Deploy

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const {ObjectID} = require("mongodb");
 const _ = require("lodash");
 const path = require("path");
 const http = require("http");
-// const socketIO = require("socket.io")
 const bodyParser = require("body-parser");
 
 const {authenticate} = require("./middleware/authenticate")
@@ -29,5 +28,15 @@ app.use("/profile/uploads", express.static("uploads"));
 app.use("/uploads", express.static("uploads"));
 app.use("/api/users", users);
 app.use("/api/rooms", rooms);
+
+// Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname,"client", "build", "index.html"));
+  })
+
+}
 
 server.listen(port, () => console.log(`Server is now listening on ${port}`));
