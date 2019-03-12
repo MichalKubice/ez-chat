f9c39c7c270b7fc4e7d4ca7752f973b319560ddf
Michal Kubiče <kubicemichal@gmail.com>
2019-03-12 16:10:17 +0100

    fuck

--- server/server.js
+++ server/server.js
@@ -11,13 +11,6 @@ const users = require("./api/users");
 const rooms = require("./api/rooms");
 
 var app = express();
-var server = http.createServer(app);
-var io = module.exports.io = require("socket.io")(server);
-io.sockets.on('connection', function(socket) {
-  console.log("Succesfull");
-
-});
-
 
 
 app.use(bodyParser.urlencoded({
@@ -38,5 +31,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-var port = process.env.port||5000;
-server.listen(port, () => console.log(`Server is now listening on ${port}`));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
